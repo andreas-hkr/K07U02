31366332eebaf143d8da831a4e0e322819f7912a
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 13:07:47 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,18 @@ public class Main {
         System.out.println("Skriv in en mening med minst två ord:");
         String sentence = input.nextLine();
 
-        int count = sentence.length();
-        int firstSpace = sentence.indexOf(' ');
-        int lastSpace = sentence.lastIndexOf(' ');
-        String firstWord = sentence.substring(0, firstSpace);
-        String lastWord = sentence.substring(lastSpace+1);
+        if (sentence.indexOf(' ') != -1) {
+            int count = sentence.length();
+            int firstSpace = sentence.indexOf(' ');
+            int lastSpace = sentence.lastIndexOf(' ');
+            String firstWord = sentence.substring(0, firstSpace);
+            String lastWord = sentence.substring(lastSpace + 1);
 
-        System.out.println("Meningen du matade in innhåller " + count + " tecken");
-        System.out.println("Det första ordet var: " + firstWord);
-        System.out.println("Det sista ordet var: " + lastWord);
+            System.out.println("Meningen du matade in innhåller " + count + " tecken");
+            System.out.println("Det första ordet var: " + firstWord);
+            System.out.println("Det sista ordet var: " + lastWord);
+        } else {
+            System.out.println("Du måste mata in minst två ord.");
+        }
     }
 }
