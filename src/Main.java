dd56dd9e1458ffe24f4156a3d68ba358d741da77
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 13:01:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Skriv in en mening med minst två ord:");
+        String sentence = input.nextLine();
+
+        int count = sentence.length();
+        int firstSpace = sentence.indexOf(' ');
+        int lastSpace = sentence.lastIndexOf(' ');
+        String firstWord = sentence.substring(0, firstSpace);
+        String lastWord = sentence.substring(lastSpace+1);
+
+        System.out.println("Meningen du matade in innhåller " + count + " tecken");
+        System.out.println("Det första ordet var: " + firstWord);
+        System.out.println("Det sista ordet var: " + lastWord);
+    }
+}
